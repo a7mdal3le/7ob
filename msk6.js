acfea2d9d25f3e8a6221080b5ea33ae01cabfa49
jekr <48930765+a7mdal3le@users.noreply.github.com>
2020-05-20 14:45:17 +0300

    Update msk6.js

--- msk6.js
+++ msk6.js
@@ -1,1 +1,18 @@
-var _0x117b=["\x68\x74\x74\x70\x73\x3A\x2F\x2F\x63\x64\x6E\x2E\x73\x74\x61\x74\x69\x63\x61\x6C\x6C\x79\x2E\x69\x6F\x2F\x67\x68\x2F\x61\x37\x6D\x64\x61\x6C\x33\x6C\x65\x2F\x37\x6F\x62\x2F\x66\x64\x39\x63\x64\x33\x33\x33\x2F\x6D\x73\x6B\x34\x2E\x6A\x73","\x67\x65\x74\x53\x63\x72\x69\x70\x74"];$[_0x117b[1]](_0x117b[0])
+$(`<img src="/sico/z1e8gf0c0610.png" class="fr" style="width:100%;margin-top: 0px;" >
+<a id="des22" style="width:95px;float:right;margin:-68px 2px 2px;color: #ffffff;transform:matrix(1, 0, 0, 1, 3, 42);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="mobile.html"> التطبيق </a>
+<a id="des22" style="width:95px;margin:-68px 2px 2px;float:right;color: #ffffff;transform:matrix(1, 0, 0, 1, 3, 15);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="sub.html">الأشتراكات</a>
+<a id="des22" style="width:90px;margin:2px;transform:matrix(1, 0, 0, 1, 6, 0);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="https://www.al3nood.net/"> شات العنود </a>
+<a id="des22" style="width:90px;margin:2px;transform:matrix(1, 0, 0, 1, 2, 0);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="https://mskchat.com/">شات الجوال</a>
+<a id="des22" style="width:92px;margin:2px;transform:matrix(1, 0, 0, 1, -2, 0);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="contact.html"> اتصل بنا </a>
+<a id="des22" style="color: #ffffff;width:97px;margin:2px;transform:matrix(1, 0, 0, 1, 8, 0);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="rules.html">القوانين</a>
+`).insertBefore('.nav-tabs');
+eval((close+'').replace('location.reload()','location.href="/"'));
+$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="" class="fl" style="margin-right: 2px; width: 25px;">');$('a[href="https://jawalhost.com/"]').removeAttr('href');
+$('.nav-tabs').addClass('fl').css('width','100%');
+$("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
+$("[data-target=#wall]").attr('title','♥ مسـاحه حـرهـ لإبداعـاتكم ♥')
+$(".checkbox label").html(`<input id="stealth" type="checkbox" value="">`);
+$('.nav-tabs').addClass('fl').css('width','100%');
+$(`<div style=" color: #FC1717;text-align: center;margin-bottom: 10px; font-family:'jazeera-light', Fontawesome"> الاعلانات/ للمسابقات فقط </div>`).insertBefore('span.fl.fa.fa-send');
+$('.stopic').attr('readonly',true).css('background-color','#eeeeee')
+var _0x3d29=["\x77\x69\x64\x74\x68","\x30\x70\x78","\x63\x73\x73","\x2E\x66\x6C\x2E\x75\x73\x74\x61\x74"];$(_0x3d29[3])[_0x3d29[2]](_0x3d29[0],_0x3d29[1])
