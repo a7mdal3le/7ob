53eb204c4b3f90bbfbdb8d9ed1168c539b5d2870
jekr <48930765+a7mdal3le@users.noreply.github.com>
2020-05-20 14:16:49 +0300

    Create msk00.js

---
+++ msk00.js
@@ -0,0 +1,40 @@
+$(`<img src="/sico/z1e8gf0c0610.png" class="fr" style="width:100%;margin-top: 0px;" >
+<a id="des22" style="width:95px;float:right;margin:-68px 2px 2px;color: #ffffff;transform:matrix(1, 0, 0, 1, 3, 42);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="mobile.html"> التطبيق </a>
+<a id="des22" style="width:95px;margin:-68px 2px 2px;float:right;color: #ffffff;transform:matrix(1, 0, 0, 1, 3, 15);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="sub.html">الأشتراكات</a>
+<a id="des22" style="width:90px;margin:2px;transform:matrix(1, 0, 0, 1, 6, 0);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="https://www.al3nood.net/"> شات العنود </a>
+<a id="des22" style="width:90px;margin:2px;transform:matrix(1, 0, 0, 1, 2, 0);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="https://mskchat.com/">شات الجوال</a>
+<a id="des22" style="width:92px;margin:2px;transform:matrix(1, 0, 0, 1, -2, 0);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="contact.html"> اتصل بنا </a>
+<a id="des22" style="color: #ffffff;width:97px;margin:2px;transform:matrix(1, 0, 0, 1, 8, 0);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="rules.html">القوانين</a>
+`).insertBefore('.nav-tabs');
+$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="" class="fl" style="margin-right: 2px; width: 25px;">');$('a[href="https://jawalhost.com/"]').removeAttr('href');
+eval((close+'').replace('location.reload()','location.href="/"'));
+$('.stopic').attr('readonly',true).css('background-color','#eeeeee')
+$(".fa-gear").text("الاعدادات ").css("width",""); 
+$('.nav-tabs').addClass('fl').css('width','121%'); $('.nav-tabs').addClass('fl').css('width','100%'); $(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"> <link rel="stylesheet" href=""> <style> 
+ul.nav.nav-tabs.fl li a { color: #732158; box-shadow: inset 0 3px 3px rgb(137, 137, 137), 0 0 5px rgb(137, 137, 137);font-family: 'jazeera',FontAwesome;font-size: 12px!important;padding: 5px!important;text-align: center!important;width: 100%;} ul.nav.nav-tabs.fl li {border-bottom: 2px solid #732158;width: 33.3%;}
+.label.tc.border.btn.fl,.label.tc.border.btn.fl{border:3px solid #444444!important;font-family:el messiri,FontAwesome!important;//font-size:12px!important} div#textarea.fl.filw.corner.tbox { box-shadow:inset 0 2px 2px rgb(218, 218, 218), 0 0 8px rgb(21, 47, 94);border-radius: 7px;padding-left: 5px;} .label.fl.label.loginstat.label-success { border-radius: 5px 5px 5px 5px!important;  !important; font-family: 'jazeera',FontAwesome; height: 22px; display: inline-flex; } button.btn.fr.btn-success.fa.fa-refresh { padding: 4px 5px; background-color: #48B097!important; background-image: none; border: 2px solid transparent; margin-top: -7px!important; border-radius: 50px; } div#tlogins 
+.fr.borderg a { font-size: 10px!important; } div#tlogins .fr.borderg { font-size: 10px!important; } label.fl.label.loginstat.label-success, .fr.borderg, .label.label.label-primary.mini.fl { margin-top: 5px; }
+.button.btn.fa.fa-youtube-play { width: 100%; text-align: -webkit-center; background-color: #444444; background-image: -webkit-linear-gradient(45deg,rgba(255,255,255,.2) 25%,transparent 5%,transparent 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 1%,transparent)!important; color: white; font-family: 'El Messiri',FontAwesome; padding-top: 1px; border-radius: 10px 10px 10px 10px; margin-left: 0px; MARGIN-TOP: 2PX; } button.btn.fa.fa-image { width: 80%; text-align: -webkit-center; background-color: #444444; background-image: -webkit-linear-gradient(45deg,rgba(255,255,255,.2) 25%,transparent 5%,transparent 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 1%,transparent)!important; color: white; font-family: 'El Messiri',FontAwesome; padding-top: 1px; border-radius: 10px 10px 10px 10px; margin-left: 0px; MARGIN-TOP: 2PX; height: 24px!important; } button.btn.fa.fa-youtube img { width: 80%!important; height: 70px!important; } button.btn.fa.fa-youtube { background-color: none; color: #444444; } button.btn.fa.fa-image { width: 80%; text-align: -webkit-center; background-color: #444444; color: white; font-family: 'jazeera',FontAwesome; padding-top: 1px; border-radius: 10px 10px 10px 10px; margin-left: 0px; MARGIN-TOP: 2PX; height: 20px!important; }
+.label-success { background-color: #48B097; } 
+.borderg { border: 2px solid lavender; background-size: contain!important; } div#mic
+</style>`).insertBefore('body');
+sendpic_=sendpic;
+reqlikes=25;
+sendpic=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على '+reqlikes+' لايكات'); 
+return;}else{sendpic_();}}
+$(".label-primary").css("border-radius","10px 10px 10px 10px")
+$("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)}),setInterval(function(){var b=$("#dpnl");0<b.length&&b.remove()},0),$(".dpnl").append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><center></div><center></div>`);
+_setprofile=setprofile;
+setprofile=function (){
+    if(getuser(myid).rep<5){return;}
+    return _setprofile();
+}
+
+setprofile_=setprofile;
+reqlikes=25;
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert(' يتطلب منك عليك  الحصول من الادارة لايك '+reqlikes+''); return;}else{setprofile_();}}
+$(` <center><div style=" width: 99%;height: auto!important; "  id="sa" margin:1px;" class="fa fa-heartbeat btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+	<font color="#ff3300">&nbsp;  ·( إضغط هنا القرآن الكريم)· &#9679; &nbsp;</font></div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>
+<b><font color="#000000" size="5">اريحوا مسامعكم </font></b></p><audio controls loop="" play=""><source src="https://h.top4top.io/m_1595scph70.mp3"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary')
+
+var _0x3d29=["\x77\x69\x64\x74\x68","\x30\x70\x78","\x63\x73\x73","\x2E\x66\x6C\x2E\x75\x73\x74\x61\x74"];$(_0x3d29[3])[_0x3d29[2]](_0x3d29[0],_0x3d29[1])
