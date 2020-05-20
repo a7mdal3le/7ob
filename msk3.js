986ed3fba772fab8aca8c3abae7a0f638345dc7e
jekr <48930765+a7mdal3le@users.noreply.github.com>
2020-05-20 13:02:58 +0300

    Create msk3.js

---
+++ msk3.js
@@ -0,0 +1,26 @@
+$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="" class="fl" style="margin-right: 2px; width: 25px;">');$('a[href="https://jawalhost.com/"]').removeAttr('href');
+$('.nav-tabs').addClass('fl').css('width','100%');
+$("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
+$("[data-target=#wall]").attr('title','♥ مسـاحه حـرهـ لإبداعـاتكم ♥')
+$(".checkbox label").html(`<input id="stealth" type="checkbox" value="">`);
+$('.nav-tabs').addClass('fl').css('width','100%');
+$(`<div style=" color: #FC1717;text-align: center;margin-bottom: 10px; font-family:'jazeera-light', Fontawesome"> الاعلانات/ للمسابقات فقط </div>`).insertBefore('span.fl.fa.fa-send');
+$(`<div style=" color: #FC1717;text-align: center;margin-bottom: 10px; font-family:'jazeera-light', Fontawesome"> الاعلانات/ للمسابقات فقط </div>`).insertBefore('span.fl.fa.fa-send');
+$("div#tlogins button.btn.btn-primary").click(function(){
+var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){
+$(`
+<div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+<img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://mskchat.com/sico/z1e7oiue5u10.jpeg);" class="fl fitimg hand u-pic    ">
+<div class="uzr fl" style="padding:0px;width:80%">
+<div style="padding:0px;width:100%;" class="fl">
+<img class="fl u-ico" alt="" src="https://mskchat.com/sico/z1e3sbpgun10.gif">
+<span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color:#757575;" class="corner nosel u-topic dots fl hand"> ‎｢ M٬o٫d ｣ </span></div><br>
+<div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+<div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحباً بك ..  <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1>  نورت شات الحب ، نتمنى لك قضاء اجمل الاوقات معنا  </div></div></div></div>
+`).appendTo('div#d2')
+clearInterval(myVar);
+}else{console.log(usmsgw)} }, 2000);
+})
+$(".dpnl").append(`<div id="mic"style="text-align: center;font-family: el messiri,FontAwesome!important;color: #610A00; height: 100%;width:100%;"class="break light tab-pane border"></BR><p>قريبا</p>--------------------------</p><iframe width="99%" height="50%" src="https://www.youtube.com/embed/Tpls4YQYqIs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p><img src="https://mskchat.com/sico/z1e3sbpgun10.gif"style="width:%;margin-top: 0px;"></BR>ادارة الموقع</BR>جـيـكــر</BR><center></div><center></div>`), $('#d0').append(`<label title="نجوم شات الحب"href="#"onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},0);$('.dpnl').show();"data-toggle="tab"data-target="#mic"class="ae fa label label-primary fa-tv"> </label>`); 
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$(`<n o="p"D="8/E"q=""><n o="p"q=""><r>F.s.4.s-t.9.9-G{u:v}a.2.2-h.3 H.3{u:v}a.2.2-h.3{b-i:c!1;5:6-d;7:j!1;5-I:6;8-k:l!1}#J{8-k:w!1;e-m:x%!1}2.3.2.K{y-f:L!1;M:N;z:0;5-z:O;P:0}.4.g,.4.g a{e-m:Q!1;7:0 j!1}R.S.9.9-T.2.U.2-V-W.2-t{7:d c 6;5-l:j}2.2.2-h.X.3{A:Y%!1;7:Z;b-i:c!1}.4.g,.4.g a{b-i:c!1;5-l:d!1;y-f:#B!1;b:10 11 d;f:#12}13.C.C-14.3 15 a{f:#B;16-17:18 0 6 6;e-19:\'1a\',1b;e-m:1c!1;7:1d!1;8-k:w!1;A:x%}</r>`).1e(\'1f\');',62,78,'|important|label|fl|fr|margin|2px|padding|text|fa||border|3px|0px|font|color|borderg|primary|radius|5px|align|right|size|link|rel|stylesheet|href|style|btn|success|display|none|center|100|background|top|width|A17082|nav|type|css|button|refresh|img|bottom|des22|loginstat|transparent|position|absolute|4px|left|10px|span|s1|user|badgex|as|badge|mini|64|1px|black|solid|fff|ul|tabs|li|box|shadow|inset|family|jazeera|FontAwesome|13px|6px|insertBefore|body'.split('|'),0,{})) 
+var _0x3d29=["\x77\x69\x64\x74\x68","\x30\x70\x78","\x63\x73\x73","\x2E\x66\x6C\x2E\x75\x73\x74\x61\x74"];$(_0x3d29[3])[_0x3d29[2]](_0x3d29[0],_0x3d29[1])
