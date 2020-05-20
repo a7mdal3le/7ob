cbdc12b1e02666ac57e2a58669b59e5aac128d1f
jekr <48930765+a7mdal3le@users.noreply.github.com>
2020-05-20 13:52:40 +0300

    Create msk7.js

---
+++ msk7.js
@@ -0,0 +1,9 @@
+$(`<img src="/sico/z1e8gf0c0610.png" class="fr" style="width:100%;margin-top: 0px;" >
+<a id="des22" style="width:95px;float:right;margin:-68px 2px 2px;color: #ffffff;transform:matrix(1, 0, 0, 1, 3, 42);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="mobile.html"> التطبيق </a>
+<a id="des22" style="width:95px;margin:-68px 2px 2px;float:right;color: #ffffff;transform:matrix(1, 0, 0, 1, 3, 15);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="sub.html">الأشتراكات</a>
+<a id="des22" style="width:90px;margin:2px;transform:matrix(1, 0, 0, 1, 6, 0);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="https://www.al3nood.net/"> شات العنود </a>
+<a id="des22" style="width:90px;margin:2px;transform:matrix(1, 0, 0, 1, 2, 0);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="https://mskchat.com/">شات الجوال</a>
+<a id="des22" style="width:92px;margin:2px;transform:matrix(1, 0, 0, 1, -2, 0);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="contact.html"> اتصل بنا </a>
+<a id="des22" style="color: #ffffff;width:97px;margin:2px;transform:matrix(1, 0, 0, 1, 8, 0);background-color: #A17082!important;" class="label label label-primary mini fl" target="_blank" href="rules.html">القوانين</a>
+`).insertBefore('.nav-tabs');
+eval((close+'').replace('location.reload()','location.href="/"'));
