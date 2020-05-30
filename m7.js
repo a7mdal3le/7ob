045409e2265fdfd0a3ceb96f3c1a1186c114ff79
jekr <48930765+a7mdal3le@users.noreply.github.com>
2020-05-31 00:38:05 +0300

    Create m7.js

---
+++ m7.js
@@ -0,0 +1,26 @@
+$(`<img src="/sico/z1d5pkn4nf10.jpg" class="fr" style="width:100%;margin-top: 0px;" ><a id="des22" style="width:95px;float:right;margin:-68px 2px 2px;transform:matrix(1, 0, 0, 1, 3, 70);" class="label label label-primary mini fl" target="_blank" href="contact.html">الادارهـ</a><a id="des22" style="width:90px;margin:2px;transform:matrix(1, 0, 0, 1, 6, 0);" class="label label label-primary mini fl" target="_blank" href="mobile.html">التطبيق</a><a id="des22" style="width:90px;margin:2px;transform:matrix(1, 0, 0, 1, 2, 0);" class="label label label-primary mini fl" target="_blank" href="sub.html">الاشتراكات</a><a id="des22" style="width:92px;margin:2px;transform:matrix(1, 0, 0, 1, -2, 0);" class="label label label-primary mini fl" target="_blank" href="rules.html">القوانين</a>`).insertBefore('.nav-tabs');$("<div class='fr borderg' style='padding:2px;background-color:white;float:left;margin-left:4px;'><a href='https://www.al3nood.net/'>شات العنود</a></div>").insertAfter($(".loginstat"));$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="" class="fl" style="margin-right: 2px; width: 25px;">');$('a[href="https://jawalhost.com/"]').removeAttr('href');
+$(".checkbox label").html(`<input id="stealth" type="checkbox" value="">`);
+eval((close+'').replace('location.reload()','location.href="/"'));
+$('.nav-tabs').addClass('fl').css('width','100%');
+$("[data-target=#wall]").attr('title','♥ مسـاحه حـرهـ لإبداعـاتكم ♥')
+$('.stopic').attr('readonly',true).css('background-color','#eeeeee')
+$("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)}),setInterval(function(){var b=$("#dpnl");0<b.length&&b.remove()},0),$(".dpnl").append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><center></div><center></div>`);
+_setprofile=setprofile;
+setprofile=function (){
+    if(getuser(myid).rep<5){return;}
+    return _setprofile();
+}
+
+setprofile_=setprofile;
+reqlikes=10;
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert(' يتطلب منك عليك  الحصول من الادارة لايك '+reqlikes+''); return;}else{setprofile_();}}
+$(` <center><div style=" width: 99%;height: auto!important; "  id="sa" margin:1px;" class="fa fa-heartbeat btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+	<font color="#ff3300">&nbsp;  ·( اذاعة شات الحب)· &#9679; &nbsp;</font></div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>
+	<b><font color="#000000" size="5">اريحوا مسامعكم </font></b></p><audio controls loop="" play=""><source src="https://h.top4top.io/m_1595scph70.mp3"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary')
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value="" maxlength="10"><i class="fa fa-eye-slash" aria-hidden="true">`)
+$(`<font size="4" color="#ffffff"><marquee direction="right" width="100%" height="20" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5" style="height: 20px; width: 100%;background-color:#47443F;"> 
+تقبل الله طاعتكم ، وكل عام وانتم بخير ، وعيدكم مبارك .. 
+</marquee></font>`).insertBefore('.nav-tabs'); 
+var _0x3d29=["\x77\x69\x64\x74\x68","\x30\x70\x78","\x63\x73\x73","\x2E\x66\x6C\x2E\x75\x73\x74\x61\x74"];$(_0x3d29[3])[_0x3d29[2]](_0x3d29[0],_0x3d29[1])
+
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$(`<n o="p"D="8/E"q=""><n o="p"q=""><r>F.s.4.s-t.9.9-G{u:v}a.2.2-h.3 H.3{u:v}a.2.2-h.3{b-i:c!1;5:6-d;7:j!1;5-I:6;8-k:l!1}#J{8-k:w!1;e-m:x%!1}2.3.2.K{y-f:L!1;M:N;z:0;5-z:O;P:0}.4.g,.4.g a{e-m:Q!1;7:0 j!1}R.S.9.9-T.2.U.2-V-W.2-t{7:d c 6;5-l:j}2.2.2-h.X.3{A:Y%!1;7:Z;b-i:c!1}.4.g,.4.g a{b-i:c!1;5-l:d!1;y-f:#B!1;b:10 11 d;f:#12}13.C.C-14.3 15 a{f:#B;16-17:18 0 6 6;e-19:\'1a\',1b;e-m:1c!1;7:1d!1;8-k:w!1;A:x%}</r>`).1e(\'1f\');',62,78,'|important|label|fl|fr|margin|2px|padding|text|fa||border|3px|0px|font|color|borderg|primary|radius|5px|align|right|size|link|rel|stylesheet|href|style|btn|success|display|none|center|100|background|top|width|A17082|nav|type|css|button|refresh|img|bottom|des22|loginstat|transparent|position|absolute|4px|left|10px|span|s1|user|badgex|as|badge|mini|64|1px|black|solid|fff|ul|tabs|li|box|shadow|inset|family|jazeera|FontAwesome|13px|6px|insertBefore|body'.split('|'),0,{})) 
